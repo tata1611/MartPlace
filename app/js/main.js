b08476c43189e294a24df31f67e9fad492f7e58d
Tata1611 <ooorubiconch@gmail.com>
2019-12-05 01:11:19 +0300

    slider start

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,31 @@
 $(function(){
 
-    var mixer = mixitup('.container');
+   
 
+    $('.slider__inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 
+  
+    // $('.slider__inner').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: 1,
+    //     adaptiveHeight: true
+    // });
+
+    $(".rate-star").rateYo({
+            rating: 5.0,
+            starWidth:"12px",
+            readOnly: true
+        });
+    
+
+    // var mixer = mixitup('.container');
     
 });
